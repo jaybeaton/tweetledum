3a14a2d518e8703d75b227e420e053b16902f2c6
Jay Beaton <jrb@jaybeaton.com>
2018-02-23 23:45:36 -0500

    add tweeter field and use user field for timeline user

--- db/tweetledum.sql
+++ db/tweetledum.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `tweetledum_tweets` (
   `id` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `user` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `tweeter` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `body` text COLLATE utf8_unicode_ci,
   `data` text COLLATE utf8_unicode_ci,
   `read` tinyint(3) unsigned NOT NULL DEFAULT '0',
