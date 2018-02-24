efb6925a4e413f10d523f3bf7bb69108ecf4c57f
Jay Beaton <jrb@jaybeaton.com>
2018-02-23 23:50:19 -0500

    put both link url and tweet url in markup

--- tweetledum/ajax.php
+++ tweetledum/ajax.php
@@ -21,11 +21,11 @@ $id = (!empty($_GET['id'])) ? $_GET['id'] : '';
 $utc = new DateTimeZone('UTC');
 $tz = new DateTimeZone('America/New_York');
 
-$sql = "SELECT id, `user`, body, `data`, `timestamp` 
-  FROM tweetledum_tweets 
-  WHERE id > ? 
+$sql = "SELECT id, `tweeter`, body, `data`, `timestamp`
+  FROM tweetledum_tweets
+  WHERE id > ?
   AND `read` = 0
-  ORDER BY id ASC 
+  ORDER BY id ASC
   LIMIT {$num_per_page} ";
 $query = $db->prepare($sql);
 
@@ -43,15 +43,15 @@ while ($row = $result->fetch_assoc()) {
   $row['data'] = unserialize($row['data']);
   $tweet_time = $date->format('Y-m-d H:i:s');
 
-  $tweet_url = 'https://twitter.com/' . $row['user'] . '/status/' . $row['id'];
-  $user_url = 'https://twitter.com/' . $row['user'];
+  $tweet_url = 'https://twitter.com/' . $row['tweeter'] . '/status/' . $row['id'];
+  $user_url = 'https://twitter.com/' . $row['tweeter'];
 
   $embed = <<<EOT
 <blockquote class="twitter-tweet" data-lang="en">
   <p lang="en" dir="ltr">
     {$row['body']}
   </p>
-  &mdash; {$row['data']['user_name']} (@{$row['user']})
+  &mdash; {$row['data']['user_name']} (@{$row['tweeter']})
   <a href="{$tweet_url}">{$tweet_time}</a>
 </blockquote>
 EOT;
@@ -61,15 +61,37 @@ EOT;
    * <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
    */
 
+  $link_url = NULL;
+  $link = NULL;
+  if (!empty($row['data']['link_url_expanded'])) {
+    $link_url = $row['data']['link_url_expanded'];
+  }
+  elseif (!empty($row['data']['link_url'])) {
+    $link_url = $row['data']['link_url'];
+  }
+  if ($link_url) {
+    $link = '<a href="' . $link_url . '">' . htmlentities($link_url) . '</a>';
+  }
+  else {
+    $link_url = $tweet_url;
+  }
+
+  if ($link) {
+    $embed = '<div class="link-url">'
+      . $link
+      . '</div>'
+      . $embed;
+  }
+
   if (!empty($row['data']['retweeted'])) {
     $embed = '<div class="retweet">'
-      . 'Retweeted by <a href="' . $user_url . '">' . htmlentities($row['data']['user_name'] . '(@' . $row['user'] . ')') . '</a>'
+      . 'Retweeted by <a href="' . $user_url . '">' . htmlentities($row['data']['user_name'] . '(@' . $row['tweeter'] . ')') . '</a>'
       . '</div>'
       . $embed;
   }
 
   print <<<EOT
-<div class="tweetledum-tweet tweetledum-new {$first_class}" id="tweetledum-{$row['id']}" data-id="{$row['id']}" data-url="{$tweet_url}">
+<div class="tweetledum-tweet tweetledum-new {$first_class}" id="tweetledum-{$row['id']}" data-id="{$row['id']}" data-url="{$link_url}" data-tweet="{$tweet_url}">
 {$embed}
 </div>
 
