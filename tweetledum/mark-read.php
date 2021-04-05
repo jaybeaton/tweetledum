841231a521afed40812f1767aceddba734ce5ffb
Jay Beaton <jrb@jaybeaton.com>
2021-04-05 00:24:44 -0400

    add list support to mark-read page

--- tweetledum/mark-read.php
+++ tweetledum/mark-read.php
@@ -16,13 +16,14 @@ if($db->connect_errno > 0){
 
 $id = (!empty($_GET['id'])) ? $_GET['id'] : '';
 $user = (!empty($_GET['user'])) ? $_GET['user'] : '';
+$list = (!empty($_GET['list'])) ? $_GET['list'] : '';
 
 $success = 0;
 
 if ($id) {
   $sql = "UPDATE tweetledum_tweets
   SET `read` = 1
-  WHERE `read` = 0 
+  WHERE `read` = 0
   AND id = ? ";
   $query = $db->prepare($sql);
   $query->bind_param('s', $id);
@@ -31,11 +32,32 @@ if ($id) {
   $success = 1;
 }
 
-$sql = "SELECT COUNT(id) as num_unread 
-  FROM tweetledum_tweets 
+$sql = "SELECT COUNT(id) as num_unread
+  FROM tweetledum_tweets
   WHERE `read` = 0 ";
 if ($user) {
   $sql .= "AND tweeter = '" . $db->real_escape_string($user) . "' ";
+}
+if ($list) {
+  // Get the tweeters in list.
+  $tweeters = [];
+  $sql2 = "SELECT data
+    FROM tweetledum_lists
+    WHERE name = ? ";
+  // @todo - We need to actually filter on the tweetledum user!
+  // AND user = ? ";
+  $query2 = $db->prepare($sql2);
+  $query2->bind_param('s', $list);
+  $query2->execute();
+  if ($list_data = $query2->get_result()->fetch_object()->data) {
+    $list_data = unserialize($list_data);
+    if (is_array($list_data)) {
+      foreach ($list_data as $tweeter) {
+        $tweeters[] = "'" . $db->real_escape_string($tweeter) . "'";
+      } // Loop thru tweeters.
+    }
+  }
+  $sql .= "AND tweeter IN (" . implode(',', $tweeters) . ") ";
 }
 
 $num_unread = $db->query($sql)->fetch_object()->num_unread;
